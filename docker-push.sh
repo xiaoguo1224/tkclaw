73a7d0a969f5afdaa9f7560e9d95dc59e4db0f8a
Alexup <yuzitongyi@qq.com>
2026-03-30 10:28:27 +0800

    更改push脚本

--- docker-push.sh
+++ docker-push.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-CURRENT_VERSION=1.2
+if [ -z "$1" ]; then
+    # 没有参数：输出错误信息到标准错误流
+    echo "ERROR: 未传入版本号参数！"
+    echo "用法: $0 <版本号>"
+    exit 1
+fi
+
+CURRENT_VERSION=$1
 
 docker tag tkclaw-llm-proxy:latest 10.0.16.26:5000/tkclaw/tkclaw-llm-proxy:v${CURRENT_VERSION}
 docker push 10.0.16.26:5000/tkclaw/tkclaw-llm-proxy:v${CURRENT_VERSION}
